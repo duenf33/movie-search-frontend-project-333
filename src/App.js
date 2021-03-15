00676321836a0f74d6d272f048d74b87c8b3382a
fernando duenas <fernando39615@aim.com>
2021-03-15 10:08:51 -0400

    commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,9 @@
-import logo from "./logo.svg";
-import "./App.css";
+import Movie from "./component/Movie";
 
 function App() {
 	return (
 		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="opener referrer">
-					Learn React
-				</a>
-			</header>
+			<Movie />
 		</div>
 	);
 }
---
+++ src/component/Movie.js
@@ -0,0 +1,61 @@
+import React, { Component } from "react";
+import axios from "axios";
+
+export class movie extends Component {
+
+  state = {
+    movieInput: "",
+    groceryList: [],
+    isToggle: false,
+    updatedInput: "",
+  };
+
+  handleMovieInputOnChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  handleMovieSubmit = async () => {
+    try {
+      let createdMovie = await axios.post(
+        "http://localhost:3001/movie/create-movie-list",
+        { movie: this.state.movieInput }
+      );
+
+      let newMovieArrayList = [
+        ...this.state.movieList,
+        createdMovie.data.data,
+      ];
+
+      this.setState({
+        movieList: newMovieArrayList,
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+	render() {
+		return (
+			<div style={{ marginTop: 20, textAlign: "center" }}>
+				<div style={{ marginTop: 20 }}>
+					<input
+						type="text"
+						name="movieInput"
+						value={this.state.movieInput}
+						onChange={this.handleMovieInputOnChange}
+					/>
+        </div>
+        <br />
+        <button style={{ marginBottom: 10 }} onClick={this.handleMovieSubmit}>
+          Submit
+        </button>
+
+        <br />
+			</div>
+		);
+	}
+}
+
+export default movie;
